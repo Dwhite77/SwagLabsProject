84a03952f2eb2acf85c14efe1d22e67e32bc1dfe
Dwhite77 <dan_white18@live.com>
2021-11-11 14:44:05 +0000

    added in product nav gherkin,stepdef and defined a method for it, only done naviagting to the baskest page

---
+++ src/test/java/com/sparta/grp1/cucumber/stepdefs/ProductsNavStepdefs.java
@@ -0,0 +1,14 @@
+package com.sparta.grp1.cucumber.stepdefs;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProductsNavStepdefs {
+    @When("I click the basket button")
+    public void iClickTheBasketButton() {
+    }
+
+    @Then("I am taken to the basket page")
+    public void iAmTakenToTheBasketPage() {
+    }
+}
