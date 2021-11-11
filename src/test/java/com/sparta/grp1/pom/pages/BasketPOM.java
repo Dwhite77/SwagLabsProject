5080402e9102bd81aba807e46fa15827ea2481b9
prl405 <73357564+prl405@users.noreply.github.com>
2021-11-11 15:57:23 +0000

    All basket tests done

--- src/test/java/com/sparta/grp1/pom/pages/BasketPOM.java
+++ src/test/java/com/sparta/grp1/pom/pages/BasketPOM.java
@@ -1,8 +1,10 @@
 package com.sparta.grp1.pom.pages;
 
-import com.sparta.grp1.cucumber.stepdefs.StepDefStateManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class BasketPOM {
     private WebDriver webDriver;
@@ -18,22 +20,27 @@ public class BasketPOM {
     }
 
     public void goToCheckoutPage() {
-//        System.out.println(w);
         webDriver.findElement(By.id("checkout")).click();
-//        System.out.println(getPageURL());
-//        return new COStepOnePOM(webDriver);
+
     }
 
     public void goToProductsPage(){
         webDriver.findElement(By.id("continue-shopping")).click();
-//        return new ProductsPOM(webDriver);
     }
 
-    private void addItemToBasket(){
+    public void addItemToBasket(){
         ProductsPOM productsPOM = new ProductsPOM(webDriver);
         goToProductsPage();
         webDriver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         productsPOM.goToBasketPage();
+    }
+
+    public String lookForBasketInItem(){
+        return webDriver.findElement(By.id("item_4_title_link")).getText();
+    }
+
+    public void removeBasketItem(){
+        webDriver.findElement(By.id("remove-sauce-labs-backpack")).click();
     }
 
     public void goToItemPage(){
@@ -42,16 +49,9 @@ public class BasketPOM {
         webDriver.findElement(By.id("item_4_title_link")).click();
     }
 
-
-
-    // -------------------------------- Helper Methods ---------------------------
-
-//    public boolean hasCheckoutPageLink(){
-//        return goToCheckoutPage().equals("https://www.saucedemo.com/checkout-step-one.html");
-//    }
-
-//    public boolean hasProductPageLink(){
-//        return goToProductsPage().getPageURL().equals("https://www.saucedemo.com/inventory.html");
-//    }
+    public int numberOfItemsInBasket(){
+        List<WebElement> webElementList = webDriver.findElements(By.id("cart_list"));
+        return webElementList.size();
+    }
 
 }
