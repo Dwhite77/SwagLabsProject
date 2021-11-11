9b2e26544516d1c03e6ec27dc02d1669dffddc28
Dwhite77 <dan_white18@live.com>
2021-11-11 14:47:22 +0000

    added in product nav gherkin,stepdef and defined a method for it, only done naviagting to the baskest page

--- src/test/java/com/sparta/grp1/cucumber/stepdefs/ProductsNavStepdefs.java
+++ src/test/java/com/sparta/grp1/cucumber/stepdefs/ProductsNavStepdefs.java
@@ -1,14 +1,32 @@
 package com.sparta.grp1.cucumber.stepdefs;
 
+import com.sparta.grp1.pom.pages.ProductsPOM;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ProductsNavStepdefs {
+
+    private static ProductsPOM productsPOM;
+    private StepDefStateManager stepDefStateManager;
+
+
+    // PicoContainer injects class ContextSteps
+    public ProductsNavStepdefs (StepDefStateManager stepDefStateManager) {
+        this.stepDefStateManager = stepDefStateManager;
+    }
+
     @When("I click the basket button")
     public void iClickTheBasketButton() {
+        productsPOM = new ProductsPOM(stepDefStateManager.getWebDriver());
+        productsPOM.goToBasketPage();
     }
 
     @Then("I am taken to the basket page")
     public void iAmTakenToTheBasketPage() {
+        stepDefStateManager.getPageURL().equals("https://www.saucedemo.com/cart.html");
     }
+
+
+
 }
--- src/test/java/com/sparta/grp1/cucumber/stepdefs/StepDefStateManager.java
+++ src/test/java/com/sparta/grp1/cucumber/stepdefs/StepDefStateManager.java
@@ -17,8 +17,8 @@ import java.io.IOException;
 
 public class StepDefStateManager {
 
-    //private static boolean initialized = false;
-    private WebDriver webDriver;
+
+    private static WebDriver webDriver;
     private static final String DRIVER_LOCATION = "src/test/resources/drivers/chromedriver.exe";
     private static ChromeDriverService service;
 
